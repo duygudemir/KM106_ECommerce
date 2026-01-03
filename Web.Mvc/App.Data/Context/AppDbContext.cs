60848663af8e9d14c1d616980769309f2fde6e65
duygudemir <duygudemir.94@gmail.com>
2026-01-03 18:28:39 +0300

    DbContext iskeleti, entities ve DbSet

---
+++ Web.Mvc/App.Data/Context/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using App.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Role> Roles => Set<Role>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Product> Products => Set<Product>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+    }
+}
