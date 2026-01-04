56a97fb31ef03002711524d5134226b91bae3385
duygudemir <duygudemir.94@gmail.com>
2026-01-04 22:15:18 +0300

    Category/List sayfasında kategoriler DB’den çekilip listelendi

--- Web.Mvc/ECommerce.Admin/Controllers/CategoryController.cs
+++ Web.Mvc/ECommerce.Admin/Controllers/CategoryController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Data.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Admin.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult List()
+        {
+            var categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
         public IActionResult Create()
         {
             return View();
