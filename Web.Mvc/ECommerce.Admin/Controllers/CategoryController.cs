e91422b915932f70b6961e755b059f23ea4eef54
duygudemir <duygudemir.94@gmail.com>
2026-01-05 16:55:24 +0300

    Listeden bir kategori seçip düzenleyebilme

--- Web.Mvc/ECommerce.Admin/Controllers/CategoryController.cs
+++ Web.Mvc/ECommerce.Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using App.Data.Context;
 using Microsoft.AspNetCore.Mvc;
+using App.Data.Entities;
+using ECommerce.Admin.Models;
+
 
 namespace ECommerce.Admin.Controllers
 {
@@ -23,9 +26,64 @@ namespace ECommerce.Admin.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(CategoryCreateVm vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var category = new Category
+            {
+                Name = vm.Name,
+                Color = vm.Color,
+                IconCssClass = vm.IconCssClass,
+                CreatedAt = DateTime.Now
+            };
+
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
         public IActionResult Edit(int id)
         {
-            return View();
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            var vm = new CategoryEditVm
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Color = category.Color,
+                IconCssClass = category.IconCssClass
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CategoryEditVm vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var category = _db.Categories.Find(vm.Id);
+
+            if (category == null)
+                return NotFound();
+
+            category.Name = vm.Name;
+            category.Color = vm.Color;
+            category.IconCssClass = vm.IconCssClass;
+
+            _db.SaveChanges();
+
+            return RedirectToAction("List");
         }
 
         public IActionResult Delete(int id)
