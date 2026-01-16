8bf97b1f26a80caf025134fa6739bdd2f36083da
duygudemir <duygudemir.94@gmail.com>
2026-01-16 17:13:16 +0300

    yorum onay

--- Web.Mvc/ECommerce.Admin/Controllers/CommentController.cs
+++ Web.Mvc/ECommerce.Admin/Controllers/CommentController.cs
@@ -1,17 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Admin.Controllers
 {
     public class CommentController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CommentController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult List()
         {
-            return View();
+            var comments = _db.ProductComments
+                .Include(c => c.Product)
+                .Include(c => c.User)
+                .OrderByDescending(c => c.Id)
+                .ToList();
+
+            return View(comments);
         }
 
         public IActionResult Approve(int id)
         {
-            return View();
+            var comment = _db.ProductComments
+                .Include(c => c.Product)
+                .Include(c => c.User)
+                .FirstOrDefault(c => c.Id == id);
+
+            if (comment == null)
+                return NotFound();
+
+            return View(comment);
+        }
+
+        [HttpPost]
+        public IActionResult ApproveConfirmed(int id)
+        {
+            var comment = _db.ProductComments.Find(id);
+
+            if (comment == null)
+                return NotFound();
+
+            comment.IsConfirmed = true;
+            _db.SaveChanges();
+
+            return RedirectToAction("List");
         }
     }
 }
