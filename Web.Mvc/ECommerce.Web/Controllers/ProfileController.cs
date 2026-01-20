41f9cfecc36b3dae686bbf8b1f16b90c6bce6de4
duygudemir <duygudemir.94@gmail.com>
2026-01-20 06:57:03 +0300

    profil sayfası eklendi

--- Web.Mvc/ECommerce.Web/Controllers/ProfileController.cs
+++ Web.Mvc/ECommerce.Web/Controllers/ProfileController.cs
@@ -1,27 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Data.Context;
+using ECommerce.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace ECommerce.Web.Controllers
 {
+    [Authorize]
     public class ProfileController : Controller
     {
-        public IActionResult Details()
+        private readonly AppDbContext _db;
+
+        public ProfileController(AppDbContext db)
         {
-            return View();
+            _db = db;
         }
 
-        public IActionResult Edit()
+        private int CurrentUserId
         {
-            return View();
+            get
+            {
+                var idText = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                return int.Parse(idText!);
+            }
         }
 
-        public IActionResult MyOrders()
+        public IActionResult Index()
         {
-            return View();
+            var user = _db.Users.FirstOrDefault(x => x.Id == CurrentUserId);
+
+            if (user == null)
+                return RedirectToAction("Login", "Account");
+
+            var model = new ProfileViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email
+            };
+
+            return View(model);
         }
 
-        public IActionResult MyProducts()
+        [HttpPost]
+        public IActionResult Index(ProfileViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = _db.Users.FirstOrDefault(x => x.Id == CurrentUserId);
+
+            if (user == null)
+                return RedirectToAction("Login", "Account");
+
+            var emailExists = _db.Users.Any(x => x.Email == model.Email && x.Id != user.Id);
+            if (emailExists)
+            {
+                ModelState.AddModelError("", "Bu email başka bir kullanıcı tarafından kullanılıyor.");
+                return View(model);
+            }
+
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Email = model.Email;
+
+            _db.SaveChanges();
+
+            ViewBag.Success = "Profil güncellendi ✅";
+            return View(model);
         }
     }
 }
