a62a7fce615c3fcc20d171ba10410adb94c18179
duygudemir <duygudemir.94@gmail.com>
2026-01-21 15:55:39 +0300

    kullanici siparis sayfası

--- Web.Mvc/ECommerce.Web/Controllers/OrderController.cs
+++ Web.Mvc/ECommerce.Web/Controllers/OrderController.cs
@@ -1,9 +1,9 @@
 ﻿using App.Data.Context;
 using App.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-
+using System.Security.Claims;
 
 namespace ECommerce.Web.Controllers
 {
@@ -17,7 +17,14 @@ namespace ECommerce.Web.Controllers
             _db = db;
         }
 
-        private int CurrentUserId => 1;
+        private int CurrentUserId
+        {
+            get
+            {
+                var idText = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                return int.Parse(idText!);
+            }
+        }
 
         public IActionResult Create()
         {
@@ -26,21 +33,20 @@ namespace ECommerce.Web.Controllers
                 .ThenInclude(ci => ci.Product)
                 .FirstOrDefault(c => c.UserId == CurrentUserId && !c.IsCompleted);
 
-            if (cart == null || !cart.CartItems.Any())
+            if (cart == null || cart.CartItems == null || !cart.CartItems.Any())
                 return RedirectToAction("Index", "Cart");
 
             var order = new Order
             {
                 UserId = CurrentUserId,
                 CreatedAt = DateTime.Now,
-                TotalPrice = cart.TotalPrice
+                TotalPrice = cart.TotalPrice,
+                OrderItems = new List<OrderItem>() 
             };
-
 
             foreach (var item in cart.CartItems)
             {
                 if (item.Product == null) continue;
-
 
                 order.OrderItems.Add(new OrderItem
                 {
@@ -49,7 +55,6 @@ namespace ECommerce.Web.Controllers
                     UnitPrice = item.Product.Price
                 });
             }
-            
 
             _db.Orders.Add(order);
 
@@ -65,7 +70,7 @@ namespace ECommerce.Web.Controllers
             var orders = _db.Orders
                 .Include(o => o.OrderItems)
                 .ThenInclude(oi => oi.Product)
-                .Where(o => o.UserId == CurrentUserId)
+                .Where(o => o.UserId == CurrentUserId) 
                 .OrderByDescending(o => o.Id)
                 .ToList();
 
