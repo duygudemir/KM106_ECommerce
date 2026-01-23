52c0647b750cc7cb079dbf7d04c2be15de7109e6
duygudemir <duygudemir.94@gmail.com>
2026-01-23 14:49:20 +0300

    parola tekrari isteme eklendi

--- Web.Mvc/ECommerce.Web/Models/RegisterViewModel.cs
+++ Web.Mvc/ECommerce.Web/Models/RegisterViewModel.cs
@@ -13,7 +13,13 @@ namespace ECommerce.Web.Models
         [Required]
         public string Email { get; set; } = "";
 
-        [Required]
+        [Required(ErrorMessage = "Parola zorunludur.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = "";
+
+        [Required(ErrorMessage = "Parola tekrarı zorunludur.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Parolalar eşleşmiyor.")]
+        public string ConfirmPassword { get; set; }
     }
 }
