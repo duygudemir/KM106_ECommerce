6b08283281b418090c1ce41d244a710400da0dca
duygudemir <duygudemir.94@gmail.com>
2026-01-24 15:55:59 +0300

    admin anasayfa duzenlendi

--- Web.Mvc/ECommerce.Admin/Controllers/HomeController.cs
+++ Web.Mvc/ECommerce.Admin/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
 using ECommerce.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using App.Data.Context;
+
 
 namespace ECommerce.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.UserCount = _db.Users.Count();
+            ViewBag.ProductCount = _db.Products.Count();
+            ViewBag.PendingCommentCount = _db.ProductComments.Count(x => !x.IsConfirmed);
+
             return View();
         }
     }
