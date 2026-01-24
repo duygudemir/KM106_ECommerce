d8e7e5a40eb543b3ba46e87eaa74e9b482d5126a
duygudemir <duygudemir.94@gmail.com>
2026-01-24 17:36:10 +0300

    admin panele logout eklendi

--- Web.Mvc/ECommerce.Admin/Controllers/ProductController.cs
+++ Web.Mvc/ECommerce.Admin/Controllers/ProductController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
 
 namespace ECommerce.Admin.Controllers
 {
